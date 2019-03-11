f4a15d3aeaca15c3e39dec07aac820279b1d15cc
Sam Becker <sam@sambecker.com>
2019-03-11 11:35:34 -0500

    Import initial source

---
+++ lib/temp/ase_builder.rb
@@ -0,0 +1,164 @@
+include BinaryService
+include HexString
+
+module ASEBuilder
+  DEFAULT_COLOR_TYPE = :global
+
+  class Palette
+    # Constructor
+
+    def initialize
+      @version_major = 1
+      @version_minor = 0
+      @colors = []
+      @groups = []
+    end
+
+    # Public methods
+
+    def version
+      "#{@version_major}.#{@version_minor}"
+    end
+
+    def set_version(major, minor)
+      @version_major = major
+      @version_minor = minor
+    end
+
+    def color_count
+      color_count = 0
+      color_count += @colors.length
+      @groups.each do |group|
+        color_count += group[:colors].length
+      end
+      color_count
+    end
+
+    def group_count
+      @groups.length
+    end
+
+    # Group management
+
+    def create_group(name)
+      unless @groups.select { |group| group[:name] == name }.length > 0
+        @groups.push({
+          name: name,
+          colors: []
+        })
+      end
+    end
+
+    def remove_group(name)
+      @groups = @groups.select { |group| group[:name] != name }
+    end 
+
+    # Color management
+
+    def add_rgb_color(name, r, g, b, type = DEFAULT_COLOR_TYPE)
+      add_color(name, :rgb, { r: r, g: g, b: b }, type)
+    end
+
+    def add_cmyk_color(name, c, m, y, k, type = DEFAULT_COLOR_TYPE)
+      add_color(name, :cmyk, { c: c, m: m, y: y, k: k }, type)
+    end
+
+    def add_lab_color(name, l, a, b, type = DEFAULT_COLOR_TYPE)
+      add_color(name, :lab, { l: l, a: a, b: b }, type)
+    end
+
+    def add_gray_color(name, gray, type = DEFAULT_COLOR_TYPE)
+      add_color(name, :gray, { gray: gray }, type)
+    end
+
+    def add_rgb_color_to_group(
+      group_name, color_name, r, g, b, type = DEFAULT_COLOR_TYPE)
+      add_color_to_group(
+        group_name, color_name, :rgb, { r: r, g: g, b: b }, type)
+    end
+
+    def add_cmyk_color_to_group(
+      group_name, color_name, c, m, y, k, type = DEFAULT_COLOR_TYPE)
+      add_color_to_group(
+        group_name, color_name, :cmyk, { c: c, m: m, y: y, k: k }, type)
+    end
+
+    def add_lab_color_to_group(
+      group_name, color_name, l, a, b, type = DEFAULT_COLOR_TYPE)
+      add_color_to_group(
+        group_name, color_name, :lab, { l: l, a: a, b: b }, type)
+    end
+
+    def add_gray_color_to_group(
+      group_name, color_name, gray, type = DEFAULT_COLOR_TYPE)
+    add_color_to_group(
+        group_name, color_name, :gray, { gray: gray }, type)
+    end
+
+    def remove_color(name)
+      @colors = @colors.select { |color| color[:name] != name }
+      @groups.each do |group|
+        group.colors = group.colors.select { |color| color[:name] != name }
+      end
+    end
+
+    # Output
+
+    def to_binary
+      palette = BinaryService.build_binary_palette(
+        @colors,
+        @groups,
+        @version_major,
+        @version_minor,
+      )
+      palette.to_binary_s
+    end
+
+    def to_hex
+      to_binary.to_hex_string
+    end 
+
+    private
+
+    def add_color(name, model, data, type)
+      unless @colors.select { |color| color[:name] == name }.length > 0
+        @colors.push({
+          name: name,
+          model: model,
+          data: data,
+          type: type,
+        })
+      end
+    end
+
+    def add_color_to_group(group_name, color_name, model, data, type)
+      found_groups = @groups.select { |group| group[:name] == group_name }
+      if found_groups.length > 0
+        group = found_groups[0]
+        unless group[:colors].select { |color| color[:name] == color_name }.length > 0
+          group[:colors].push({
+            name: color_name,
+            model: model,
+            data: data,
+            type: type,
+          })
+        end
+      end
+    end  
+
+    def get_group(name)
+      found_groups = @groups.select { |group| group[:name] == name }
+      found_groups.length > 0 ? found_groups[0] : nil
+    end
+
+    def get_color_from_group(group_name, color_name)
+      group = get_group(group_name)
+      if group
+        found_colors = group[:colors].select { |color| color[:name] == name }
+        found_colors.length > 0 ? found_colors[0] : nil
+      else
+        nil
+      end
+    end
+  end
+end
---
+++ lib/temp/binary_service.rb
@@ -0,0 +1,213 @@
+require 'bindata'
+require 'hex_string'
+
+module BinaryService
+  # Constants
+
+  DEFAULT_VERSION_MAJOR   = 1
+  DEFAULT_VERSION_MINOR   = 0
+
+  BLOCK_TYPE_COLOR        = 0x1
+  BLOCK_TYPE_GROUP_START  = 0xC001
+  BLOCK_TYPE_GROUP_END    = 0xC002
+  
+  COLOR_TYPE_GLOBAL       = 0
+  COLOR_TYPE_SPOT         = 1
+  COLOR_TYPE_NORMAL       = 2
+  COLOR_TYPE_DEFAULT      = COLOR_TYPE_NORMAL
+
+  COLOR_MODEL_RGB         = "RGB "
+  COLOR_MODEL_CMYK        = "CMYK"
+  COLOR_MODEL_LAB         = "LAB "
+  COLOR_MODEL_GRAY        = "GRAY"
+  COLOR_MODEL_DEFAULT     = COLOR_MODEL_RGB
+
+  def build_binary_palette(
+    colors,
+    groups,
+    version_major = DEFAULT_VERSION_MAJOR,
+    version_minor = DEFAULT_VERSION_MINOR
+  )
+    palette = ASEBinData.new
+    palette.version_major = version_major
+    palette.version_minor = version_minor
+    colors.each do |color|
+      binary_add_color(
+        palette,
+        color[:name],
+        color[:model],
+        color[:type],
+        color[:data],
+      )
+    end
+    groups.each do |group|
+      binary_begin_group(palette, group[:name])
+      group[:colors].each do |color|
+        binary_add_color(
+          palette,
+          color[:name],
+          color[:model],
+          color[:type],
+          color[:data],
+        )
+      end
+      binary_end_group(palette)
+    end
+    palette
+  end
+
+  private
+
+  def binary_add_color(palette, name, model, type, data)
+    case model
+    when :rgb
+      color_model = COLOR_MODEL_RGB
+      color_data = {
+        red: data[:r] / 255.0,
+        green: data[:g] / 255.0,
+        blue: data[:b] / 255.0,
+      }
+    when :cmyk
+      color_model = COLOR_MODEL_CMYK
+      color_data = {
+        cyan: data[:c] / 100.0,
+        magenta: data[:m] / 100.0,
+        yellow: data[:y] / 100.0,
+        black: data[:k] / 100.0,
+      }
+    when :lab
+      color_model = COLOR_MODEL_LAB
+      color_data = {
+        lightness: data[:l] / 100.0,
+        a: data[:a],
+        b: data[:b],
+      }
+    when :gray
+      # Grayscale is no longer supported by Adobe (was it ever?)
+      # This will default to a CMYK value with data only for black
+      color_model = COLOR_MODEL_CMYK
+      color_data = {
+        cyan: 0.0,
+        magenta: 0.0,
+        yellow: 0.0,
+        black: data[:gray] / 100.0,
+      }
+    end
+
+    case type
+    when :global
+      color_type = COLOR_TYPE_GLOBAL
+    when :spot
+      color_type = COLOR_TYPE_SPOT
+    when :normal
+      color_type = COLOR_TYPE_NORMAL
+    end
+
+    palette.blocks.push({
+      block_type: BLOCK_TYPE_COLOR,
+      block_data: {
+        name: name.encode!(Encoding::UTF_16BE),
+        color_model: color_model,
+        color_data: color_data,
+        color_type: color_type,
+      }
+    })
+  end
+
+  def binary_begin_group(palette, name)
+    palette.blocks.push({
+      block_type: BLOCK_TYPE_GROUP_START,
+      block_data: {
+        name: name.encode!(Encoding::UTF_16BE),
+      }
+    })
+  end
+
+  def binary_end_group(palette)
+    palette.blocks.push({
+      block_type: BLOCK_TYPE_GROUP_END,
+    })
+  end
+
+  class ASEBinData < BinData::Record
+    endian :big
+  
+    string :signature, value: "ASEF"
+    uint16 :version_major, initial_value: DEFAULT_VERSION_MAJOR
+    uint16 :version_minor, initial_value: DEFAULT_VERSION_MINOR
+    uint32 :block_count, value: -> { blocks.length }
+  
+    array :blocks do
+      uint16 :block_type
+      uint32 :block_length, value: -> {
+        block_length = 0
+        if block_type == BLOCK_TYPE_COLOR ||
+           block_type == BLOCK_TYPE_GROUP_START
+          block_length += block_data.name.length
+        end
+        if block_type == BLOCK_TYPE_COLOR
+          block_length += 10
+          case block_data.color_model
+          when COLOR_MODEL_RGB
+            block_length += 4 * 3
+          when COLOR_MODEL_CMYK, COLOR_MODEL_GRAY
+            block_length += 4 * 4
+          when COLOR_MODEL_LAB
+            block_length += 4 * 3
+          end
+        elsif block_type == BLOCK_TYPE_GROUP_START
+          block_length += 4
+        end
+        block_length
+      }
+      choice :block_data, selection: -> { block_type } do
+        class BlockGroupStart < BinData::Record
+          endian :big
+          uint16 :name_length, value: -> { name.length / 2 + 1 }
+          string :name, read_length: :name_length
+          uint16 :null_padding, value: 0
+        end
+        class BlockGroupEnd < BinData::Record
+        end
+        class BlockColor < BinData::Record
+          endian :big
+          uint16 :name_length, value: -> { name.length / 2 + 1 }
+          string :name, read_length: :name_length
+          uint16 :null_padding, value: 0
+          string :color_model, initial_value: COLOR_MODEL_DEFAULT
+          choice :color_data, selection: -> { color_model } do
+            class ColorDataRGB < BinData::Record
+              endian :big
+              float :red, initial_value: 0
+              float :green, initial_value: 0
+              float :blue, initial_value: 0
+            end
+            class ColorDataCMYK < BinData::Record
+              endian :big
+              float :cyan, initial_value: 0
+              float :magenta, initial_value: 0
+              float :yellow, initial_value: 0
+              float :black, initial_value: 0
+            end
+            class ColorDataLAB < BinData::Record
+              endian :big
+              float :lightness, initial_value: 0
+              float :a, initial_value: 0
+              float :b, initial_value: 0
+            end
+  
+            ColorDataRGB  COLOR_MODEL_RGB
+            ColorDataCMYK COLOR_MODEL_CMYK
+            ColorDataLAB  COLOR_MODEL_LAB
+            # Grayscale data is stored in a CMYK structure
+            ColorDataCMYK COLOR_MODEL_GRAY
+          end
+          uint16 :color_type, initial_value: COLOR_TYPE_DEFAULT
+        end
+        BlockGroupStart  BLOCK_TYPE_GROUP_START
+        BlockGroupEnd    BLOCK_TYPE_GROUP_END
+        BlockColor       BLOCK_TYPE_COLOR
+      end
+    end
+  end
+end
