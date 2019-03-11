d98af0a97379896b2dd1b10c6b12538da3235367
Sam Becker <sam@sambecker.com>
2019-03-11 11:49:42 -0500

    Refactor test colors

---
+++ spec/ase_palette_colors.rb
@@ -0,0 +1,75 @@
+# RGB
+COLOR_VIOLET_RGB = [
+  "Violet RGB",
+  :rgb,
+  { r: 50, g: 100, b: 255 },
+  :spot,
+]
+COLOR_BLUE_RGB = [
+  "Blue RGB",
+  :rgb,
+  { r: 0, g: 0, b: 255 },
+  :spot,
+]
+COLOR_GREEN_RGB = [
+  "Green RGB",
+  :rgb,
+  { r: 50, g: 255, b: 50 },
+  :spot,
+]
+COLOR_RED_RGB = [
+  "Red RGB",
+  :rgb,
+  { r: 240, g: 0, b: 20 },
+  :spot,
+]
+
+# CMYK
+COLOR_VIOLET_CMYK = [
+  "Violet CMYK",
+  :cmyk,
+  { c: 71, m: 65, y: 0, k: 0 },
+  :spot,
+]
+COLOR_BLUE_CMYK = [
+  "Blue CMYK",
+  :cmyk,
+  { c: 71, m: 10, y: 0, k: 0 },
+  :spot,
+]
+COLOR_GREEN_CMYK = [
+  "Green CMYK",
+  :cmyk,
+  { c: 100, m: 0, y: 50, k: 0 },
+  :spot,
+]
+COLOR_RED_CMYK = [
+  "Red CMYK",
+  :cmyk,
+  { c: 0, m: 100, y: 100, k: 0 },
+  :spot,
+]
+
+# LAB
+COLOR_RED_LAB = [
+  "Red LAB",
+  :lab,
+  { l: 52, a: 73, b: 53},
+  :spot,
+]
+
+# GRAY
+COLOR_GRAY = [
+  "GRAY",
+  :gray,
+  { gray: 65 },
+  :spot,
+]
+
+# PANTONE
+COLOR_PANTONE_117_C = [
+  "PANTONE 117 C",
+  :lab,
+  { l: 66.27451181411743, a: 12, b: 79},
+  :spot,
+]
