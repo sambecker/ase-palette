3f9e563c0ee8ff9fdb1e163abc0516f60c309aae
Sam Becker <sam@sambecker.com>
2019-03-12 23:13:15 -0500

    Add and test group class behavior

--- lib/ase_palette/palette_v2.rb
+++ lib/ase_palette/palette_v2.rb
@@ -1,7 +1,7 @@
 module ASEPalette
   class PaletteV2
 
-    # Constructor
+    # Palette constructor
     
     def initialize
       @version_major = 1
@@ -10,7 +10,7 @@ module ASEPalette
       @groups = []
     end
 
-    # Version
+    # Palette version
 
     def version
       "#{@version_major}.#{@version_minor}"
@@ -21,28 +21,39 @@ module ASEPalette
       @version_minor = minor
     end
 
-    # Accessors
+    # Color accessors
 
     def colors
       @colors.clone
     end
 
-    def get_color(name)
-      all_colors = get_all_colors
-      found_color = all_colors.select { |c| c.name == name }
-      found_color.length >= 1 ? found_color[0] : nil
+    def get_color_by_name(name)
+      found_colors = get_all_colors.select { |c| c.name == name }
+      found_colors.length >= 1 ? found_colors[0] : nil
     end
+
+    # Group accessors
 
     def groups
       @groups.clone
     end
 
+    def get_group_by_name(name)
+      found_groups = @groups.select { |g| g.name == name }
+      found_groups.length >= 1 ? found_groups[0] : nil
+    end
+
     # Mutators
 
-    def add_color(color)
+    def add_color(color, group_name = nil)
       if color.is_a? Color
-        if @colors.select { |c| c.name != color.name }.length == 0
-          @colors << color
+        if color_does_not_exist_in_palette(color.name)
+          if group_name
+            group = find_or_create_group(group_name)
+            group.colors << color
+          else
+            @colors << color
+          end
         else
           raise Error, "A color with that name already exists"
         end
@@ -51,19 +62,21 @@ module ASEPalette
       end
     end
 
-    def add_group(group)
-      if group.is_a? Group
-        if @groups.select { |g| g.name != group.name }.length == 0
-          @groups << group
-        else
-          raise Error, "A group with that name already exists"
-        end
+    def create_group(name)
+      if @groups.select { |g| g.name != group.name }.length == 0
+        new_group = ASEPalette::Group.new(name)
+        @groups << new_group
+        new_group
       else
-        raise Error, "Argument is not of type #{ASEPalette::Group}"
+        raise Error, "A group with that name already exists"
       end
     end
 
     private
+
+    def color_does_not_exist_in_palette(name)
+      get_all_colors.select { |c| c.name == name }.length == 0
+    end
 
     def get_all_colors
       colors_all = @colors
@@ -71,6 +84,15 @@ module ASEPalette
       @groups.each { |group| colors_all += group.colors }
       # puts colors_all
       colors_all
+    end
+
+    def find_or_create_group(name)
+      found_groups = @groups.select { |g| g.name == name }
+      if found_groups.length > 0
+        found_groups[0]
+      else
+        create_group(name)
+      end
     end
   end
 end
--- spec/ase_palette_new_spec.rb
+++ spec/ase_palette_new_spec.rb
@@ -28,20 +28,28 @@ RSpec.describe ASEPalette do
     context "adds" do
       it "rgb color" do
         @palette.add_color ASEPalette::Color::RGB.new "Red", 255, 0, 0
-        saved_color = @palette.get_color("Red")
+        saved_color = @palette.get_color_by_name("Red")
         expect(saved_color.data).to eq({ r: 255, g: 0, b: 0 })
       end
       it "cmyk color" do
         @palette.add_color ASEPalette::Color::CMYK.new "Cyan", 100, 0, 0, 0
-        saved_color = @palette.get_color("Cyan")
+        saved_color = @palette.get_color_by_name("Cyan")
         expect(saved_color.data).to eq({ c: 100, m: 0, y: 0, k: 0 })
+      end
+      it "color to group" do
+        color = ASEPalette::Color::RGB.new "Red", 255, 0, 0
+        expect(@palette.colors.length).to eq 0
+        expect(@palette.groups.length).to eq 0
+        @palette.add_color color, "New Group"
+        expect(@palette.colors.length).to eq 0
+        expect(@palette.groups.length).to eq 1
       end
     end
 
     context "builds" do
       it "binary hash" do
         @palette.add_color ASEPalette::Color::CMYK.new "Cyan", 100, 0, 0, 0
-        saved_color = @palette.get_color("Cyan")
+        saved_color = @palette.get_color_by_name("Cyan")
         expect(saved_color.to_object).to eq({
           name: "Cyan",
           model: :cmyk,
